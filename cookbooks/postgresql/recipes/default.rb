aec3e908ab6bd2acb7502c30dbb1a88c0a4e186d
ashutoshvct <ashutosh.vct@gmail.com>
2017-04-11 12:07:43 +0000

    new cookbook psql

---
+++ cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook:: postgresql
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'postgresql-server' do
+	notifies :run, 'execute[postgresql-init]'
+end
+
+execute 'postgresql-init' do
+	command 'postgresql-setup initdb'
+	action :nothing
+end
+
+service 'postgresql' do
+	action [:enable, :start]
+end
+
+
+
+
